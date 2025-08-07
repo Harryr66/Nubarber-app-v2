ef5f87a132febf10a5f8139a554fb32aafedcb91
Harry <hrollerson1@gmail.com>
2025-08-07 10:01:49 +0000

    Errors persist

--- src/lib/payments.ts
+++ src/lib/payments.ts
@@ -4,7 +4,7 @@
 import Stripe from "stripe";
 import { headers } from "next/headers";
 import { getFirebase } from "./firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
